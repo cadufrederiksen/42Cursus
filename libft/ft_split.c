98ba09a4084a8d87206dcf0d8b52e888da4eb9b1
cadufrederiksen <cadu.riksen@gmail.com>
2023-05-02 17:12:40 +0200

    split 100%

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,46 +6,47 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 11:25:54 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/05/02 13:28:09 by carmarqu         ###   ########.fr       */
+/*   Updated: 2023/05/02 17:10:15 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
-int countword(char const *s, char c)
+int	countword(char const *s, char c)
 {
-	size_t i;
-	size_t counter;
+	size_t	i;
+	size_t	counter;
 
 	counter = 0;
 	i = 0;
-	while (s[i] == c && s[i])
-		i++;
-	while (s[i])
+	while (s[i] != '\0')
 	{
-		counter++;
-		while (s[i] == c && s[i])
+		if (s[i] == c && s[i] != '\0')
 			i++;
-		while (s[i] != c && s[i])
-			i++;
+		else
+		{
+			counter++;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+		}
 	}
-	return(counter);
+	return (counter);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	int count_word;
-	char **ptr;
-	size_t x;
-	size_t len;
+	size_t	count_word;
+	size_t	x;
+	size_t	len;
+	char	**ptr;
 
-	x = 0;
-	
 	count_word = countword(s, c);
-	ptr = (char**)malloc(count_word + 1);
-	if (!ptr)
+	if (!s)
 		return(0);
+	ptr = (char **)malloc(sizeof(char *) * (countword(s, c) + 1));
+	if (!ptr)
+		return (0);
+	x = 0;
 	while (*s)
 	{
 		if (*s == c)
@@ -53,31 +54,34 @@ char **ft_split(char const *s, char c)
 		else
 		{
 			len = 0;
-			while(*s != c)	
-			{
-				s++;
+			while (*(s + len) != c && *(s + len) != 0)
 				len++;
-			}
+			if (x < count_word)
+			{
 				ptr[x] = ft_substr(s, 0, len);
 				x++;
+			}
+			s += len;
 		}
-	}	
-	ptr[count_word] = NULL;
-	return(ptr); 
-} 
-
-int main()
-{
-	char s[] = "hola que tal";
-	char c = ' ';
-	int x;
-
-	x = 0;
-	while (x <= countword(s, c))
- 	{
-		printf("%s\n", ft_split(s, c)[x]);
-		x++;
 	}
-	return(0);
+	ptr[x] = NULL;
+	return (ptr); 
 }
-//hyfevffe
+
+/* int	main(void)
+{
+	char	s[]= "hola que tal";
+	char	c;
+	char **result;
+	c = ' ';
+
+	result = ft_split(s,c);
+	
+
+		printf("%s\n", result[0]);
+		printf("%s\n", result[1]);
+		printf("%s\n", result[2]);
+		printf("%s", result[3]);
+	return (0);
+}
+ */
