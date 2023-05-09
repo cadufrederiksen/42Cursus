6788153adf9acb37856ccdb5436aa2a3281a36e2
Your Name <you@example.com>
2023-05-09 17:35:04 +0200

    wadwad

--- libft/teste.c
+++ libft/teste.c
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   teste.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/17 14:04:45 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/05/09 17:32:15 by marvin           ###   ########.fr       */
+/*   Created: 2023/05/09 17:32:42 by marvin            #+#    #+#             */
+/*   Updated: 2023/05/09 17:32:42 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int	ft_isalnum(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	else if (c >= 'a' && c <= 'z')
-		return (1);
-	else if (c >= 'A' && c <= 'Z')
-		return (1);
-	return (0);
-}
-
-/* int main ()
-{
-    unsigned char c;
-
-    c = 'd';
-    printf("%i", ft_isalnum(c));
-} */
