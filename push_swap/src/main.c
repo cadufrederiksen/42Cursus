235bddf9484d56cbd8b1267600e1ef14929f3c8e
cadufrederiksen <cadu.riksen@gmail.com>
2023-08-22 11:55:43 +0200

    agora foi

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: carmarqu <carmarqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 12:35:16 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/08/22 11:11:20 by carmarqu         ###   ########.fr       */
+/*   Updated: 2023/07/25 12:07:38 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 {
 	int checker;
 	checker = argc;
-	if(argc <= 1)
+	if(argc <= 1)	
 		return (0);
 	if (argc == 2)
 	{
