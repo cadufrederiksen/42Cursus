d5cab4d7c9ba2d6a2dc8e5632f0d532f3019bf7f
cadufrederiksen <cadu.riksen@gmail.com>
2023-08-30 14:08:18 +0200

    stuff add

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,13 +6,18 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 14:23:13 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/08/29 16:40:16 by carmarqu         ###   ########.fr       */
+/*   Updated: 2023/08/30 13:20:55 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "libft.h"
+#include "ft_printf.h"
+#include <signal.h>
 
 int main(int argc, char **argv)
 {
+	if (argc != 2)
+		return (0);
+	kill((pid_t)argv[1], SIGUSR1);
 	return(0);
 }
--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -6,16 +6,21 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 14:23:15 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/08/29 17:25:40 by carmarqu         ###   ########.fr       */
+/*   Updated: 2023/08/30 13:19:48 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "libft.h"
+#include "ft_printf.h"
+#include <signal.h>
 
 int main(void)
 {
 	int pid;
 	
 	pid = getpid();
+	ft_printf("%d\n", pid);
+	pause();
+	ft_printf("%s\n", "voltei");
 	return(0);
 }
