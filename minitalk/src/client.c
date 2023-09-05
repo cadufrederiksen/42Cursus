3a3e2786edd4425ee17f086a131e6bff21d08c8d
cadufrederiksen <cadu.riksen@gmail.com>
2023-09-05 16:42:34 +0200

    stuff add

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,7 +6,7 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 14:23:13 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/09/04 15:08:17 by carmarqu         ###   ########.fr       */
+/*   Updated: 2023/09/05 16:12:08 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,68 @@
 #include "ft_printf.h"
 #include <signal.h>
 
+void send_sig(char *byte, pid_t pid)
+{
+	int i;
+	
+	i = 0;
+	while(byte[i] != '\0')
+	{
+		if(byte[i] == '1')
+		{
+			ft_printf("%d\n", kill(pid, SIGUSR2));
+			write(1, "sent 2\n", 7);
+		}
+		else if (byte[i] == '0')
+		{
+			ft_printf("%d\n", kill(pid, SIGUSR1));
+			write(1, "sent 1\n", 7);
+		}
+		i++;
+	} 
+}
+
+void letter_to_byte(int letter, char *byte, pid_t pid)
+{
+	int i;
+	int flag;
+	
+	flag = 1;
+	i = 7;
+	while (i >= 0)
+	{
+		if (letter == 1 && flag == 0)
+			byte[i--] = '0';
+		else if (letter == 1 && flag == 1)
+		{
+			byte[i--] = '1';
+			flag--;
+		}
+		else
+		{
+			byte[i] = (letter % 2) + 48;
+			letter = letter / 2;
+			i--;
+		}
+	}
+	byte[8] = '\0';
+	send_sig(byte, pid);
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 3)
-		return (0);
+		exit(0);
 	pid_t pid;
-	int result;
 	char *string;
 	int i;
-
+	char *byte;
+	
+	byte = (char *)malloc(9);
 	i = 0;
-	string = (char *)malloc(ft_strlen(argv[2]) + 1);//talvez o +1 nao precise
-	while(argv[2][i] != '\0')
-	{
-		string[i] = argv[2][i];
-		i++;
-	}
-	string[i] = '\0';
-	ft_printf("%s\n", string);
+	string = argv[2];
 	pid = ft_atoi(argv[1]);
-	result = kill(pid, SIGUSR1);
+	while(string[i])
+		letter_to_byte(string[i++], byte, pid);
 	return (0);
 }
--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -6,7 +6,7 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 14:23:15 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/09/04 16:53:12 by carmarqu         ###   ########.fr       */
+/*   Updated: 2023/09/05 16:26:40 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,71 @@
 #include "ft_printf.h"
 #include <signal.h>
 
-void handler (int sig)
+int	ft_recursive_power(int nb, int power)
 {
-	ft_printf("signal: %d\n", sig);
-	exit(0);
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
+}
+
+int byte_to_letter(char *byte)
+{
+	char result;
+	int i;
+	
+	i = 0;
+	result = 0;
+	while(i < 8)
+	{
+		if (byte[i] == '0')
+			i++;
+		else if(byte[i] == '1')
+		{
+			result += ft_recursive_power(2, 7 - i);
+			i++;
+		}
+	}
+	write(1, &result, 1);
+	return(0);
+}
+
+void handler (int sig)//descobrir como fazer um loop
+{
+	char *byte;
+	static int i;
+	
+	i = 0;
+	
+	byte = (char*)malloc(9);
+	if(sig == SIGUSR1)
+		byte[i++] = '0';
+	else 
+		byte[i++] = '1';
+	byte[8] = '\0';
+	if (i == 8)
+	{
+		ft_printf("%s", "hello");
+		byte_to_letter(byte);
+		i = 0;
+	}
 }
 
 int main(void)
 {
 	int pid;
-	
+	struct sigaction sa;
+
 	pid = getpid();
 	ft_printf("Pid es: %d\n", pid);
-	signal(SIGUSR1, handler);
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	pause();
+	
 	return (0);
 }
