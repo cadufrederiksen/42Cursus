c1344519e315036a2b4b03e0f18696f1c1bd87ca
cadufrederiksen <cadu.riksen@gmail.com>
2023-09-18 15:44:39 +0200

    minitalk 125

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,31 +6,11 @@
 /*   By: carmarqu <carmarqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 14:23:13 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/09/15 16:49:28 by carmarqu         ###   ########.fr       */
+/*   Updated: 2023/09/18 15:42:07 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-void send_len (pid_t pid, int strlen)
-{
-	static int i = 0;//bits recebidos
-	static int len = 0; //str len
-	
-	if(!len)
-		len = strlen;
-	i = -1;
-	if(i < 32)
-	{
-		if(len & 1)
-			kill(pid, SIGUSR1);
-		else 
-			kill(pid, SIGUSR2);
-		len = len >> 1;
-		i++;
-		usleep(100);
-	} 
-}
 
 void send_null(pid_t pid)
 {
@@ -39,6 +19,7 @@ void send_null(pid_t pid)
 	i = 0;
 	if(i <= 8)
 		kill(pid, SIGUSR2);
+	i++;
 }
 
 void send_char(pid_t pid, char *string)
@@ -46,26 +27,19 @@ void send_char(pid_t pid, char *string)
 	static int bits = 0;//bits recebidos
 	static char *str = 0; //string a ser devolvida
 
-	bits = 0;
 	if (string)
 		str = string;
-	ft_printf("%s\n", "aqui");
 	if(*str)
 	{
-		if(bits < 8)
-		{
-			if((unsigned char)*str & 1)
-				kill(pid, SIGUSR1);//envia 1
-			else 
-				kill(pid, SIGUSR2);	//envia 0
-			*str = *str >> 1;
-			bits++; 
-			usleep(100);
-		} 
-		if (bits == 7)
+		if((*str >> bits) & 1)
+			kill(pid, SIGUSR1);//envia 1
+		else
+			kill(pid, SIGUSR2);	//envia 0
+		bits++; 
+		if (bits == 8)
 		{
 			bits = 0;
-			(*str)++;
+			str++;
 		}	
 	}
 	if(!*str)
@@ -76,12 +50,13 @@ void send_info(int sig, siginfo_t *sa, void *context)
 {
 	static pid_t pid = 0;
 	
-	pid = sa->si_pid;
+	if(sa->si_pid != 0)
+		pid = sa->si_pid;
 	(void)context;
 	if (sig == SIGUSR1)
-		send_len(pid, 0);
-	else if (sig == SIGUSR2)
 		send_char(pid, NULL);
+	else if (sig == SIGUSR2)
+		exit(0);
 }
 
 int main(int argc, char **argv)
@@ -97,7 +72,6 @@ int main(int argc, char **argv)
 	string = argv[2];
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	send_len(ft_atoi(argv[1]), ft_strlen(string));//enviar para salvar as variaveis
 	send_char(ft_atoi(argv[1]), string);//enviar para salvar as variaveis
 	while(1)
 		usleep(100);
--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -6,7 +6,7 @@
 /*   By: carmarqu <carmarqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 14:23:15 by carmarqu          #+#    #+#             */
-/*   Updated: 2023/09/15 16:37:57 by carmarqu         ###   ########.fr       */
+/*   Updated: 2023/09/18 15:36:47 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_recursive_power(int nb, int power)
 		return (nb * ft_recursive_power(nb, power - 1));
 }
 
-
+/* 
 void get_len(int *len_flag, int *bits_received, char **string, siginfo_t *sa)
 {
 	static int len = 0;
@@ -42,56 +42,40 @@ void get_len(int *len_flag, int *bits_received, char **string, siginfo_t *sa)
 	usleep(100);
 	(*bits_received)++;	
 }	
-
-void return_string(char **string, int *index, int *len_flag)
-{
-	*len_flag = 0;
-	if(*string)
-	{
-		ft_putendl_fd(*string, 1);
-		free(*string);
-		string = 0;
-	}
-	*index = 0;
-}
+ */
 
 void get_info (int sig, siginfo_t *sa, void *context)
 {
-	static char *string = 0;//string a ser devolvida 
-	static int index = 0;//posicao na string
-	static int len_flag = 0;//se recebeu ou nao len
 	static int byte_char = 0;//char em numero
 	static int bits_received = 0;//quantidade de bits recebidos
 	int client_pid;
 	
 	(void)context;
 	client_pid = sa->si_pid;
-	if(!len_flag)
-		get_len(&len_flag, &bits_received, &string, sa);
-	else 
+	if (sig == SIGUSR1)
+		byte_char += ft_recursive_power(2, bits_received);
+	bits_received++;
+	if(bits_received == 8)
 	{
-		if (sig == SIGUSR1)
-			byte_char += ft_recursive_power(2, bits_received);
-		if(bits_received == 7)
-		{//bits recebidos
-			string[index++] = byte_char;
-			bits_received = 0;
-			if (byte_char == 0)
-				return (return_string(&string, &index, &len_flag));				
-			byte_char = 0;
-			return ;
+		bits_received = 0;
+		if (byte_char == 0)
+		{
+			write(1, "\n", 1);
+			kill(client_pid, SIGUSR2);
 		}
-		kill(client_pid, SIGUSR2);
-		usleep(100);
-		bits_received++;
+		if(byte_char != 0)
+			write(1, &byte_char, 1);
+		byte_char = 0;
 	}
+	kill(client_pid, SIGUSR1);
+	usleep(100);
 }
 
 int main(void)
 {
 	struct sigaction	sa;
-	sa.sa_sigaction = get_info;
 	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = get_info;
 	sigemptyset(&sa.sa_mask);
 	int pid;
 	
