a4bee1611472708a29d8037b1b6a9fd61328f1fd
cadufrederiksen <cadu.riksen@gmail.com>
2023-11-15 12:11:10 +0100

    philo add

---
+++ philo/Phillospher.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phillospher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 11:05:30 by carmarqu          #+#    #+#             */
+/*   Updated: 2023/11/15 11:06:55 by carmarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHER_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
