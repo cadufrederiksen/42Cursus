416a314d941eb5649e9d100052963e8643643371
cadufrederiksen <cadu.riksen@gmail.com>
2024-01-15 16:53:10 +0100

    stuff add

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: carmarqu <carmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:19:24 by carmarqu          #+#    #+#             */
-/*   Updated: 2024/01/11 14:53:13 by carmarqu         ###   ########.fr       */
+/*   Updated: 2024/01/15 16:53:01 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 int main()
 {
-	readline("hola");
+	char *input;
+	
+	input = readline("hola");
+	//rl_replace_line(input, 0);
+	printf("%s", input);
 	rl_redisplay();
-	//rl_replace_line("hola de nuevo", 0);
 }
