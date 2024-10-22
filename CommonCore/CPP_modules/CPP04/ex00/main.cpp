21d17059bd9f1cd031bc9e8d947397778366d102
cadufrederiksen <cadu.riksen@gmail.com>
2024-10-22 18:21:50 +0200

    stuff add

--- CommonCore/CPP_modules/CPP04/ex00/main.cpp
+++ CommonCore/CPP_modules/CPP04/ex00/main.cpp
@@ -6,19 +6,22 @@
 /*   By: carmarqu <carmarqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 20:19:46 by carmarqu          #+#    #+#             */
-/*   Updated: 2024/10/21 20:38:50 by carmarqu         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:48:16 by carmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 #include "Dog.hpp"
 
 int main()
 {
 const Animal* meta = new Animal();
 const Animal* j = new Dog();
-const Animal* i = new Cat();
+const WrongAnimal* i = new WrongCat();
+//const Animal* x = new Cat();
 std::cout << j->getType() << " " << std::endl;
 std::cout << i->getType() << " " << std::endl;
 i->makeSound(); //will output the cat sound!
